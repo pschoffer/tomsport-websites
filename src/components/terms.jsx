d2753ce700f45c34f9281abe47c553ce41b0e1fd
Pavel Schoffer <xschof00@gmail.com>
2019-07-24 06:13:57 +0200

    Terms filled

--- src/components/terms.jsx
+++ src/components/terms.jsx
@@ -3,12 +3,20 @@ import texts from "../text.json";
 
 class Terms extends Component {
   state = {};
+
+  renderItem(item) {
+    return <li key={item}>{item}</li>;
+  }
+
   render() {
     return (
       <div>
         <h1 className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
           {texts.main.terms}
         </h1>
+        <ul className="text-left">{texts.terms.base.map(this.renderItem)}</ul>
+        <h2>{texts.terms.return_title}</h2>
+        <ul className="text-left">{texts.terms.return.map(this.renderItem)}</ul>
       </div>
     );
   }
